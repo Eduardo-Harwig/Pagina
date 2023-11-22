45647f16adf5b2d1ab46f1ba781054b402af34c9
Eduardo Hartwig <eduardohartwigiveira@gmail.com>
2023-11-22 04:59:59 -0300

    initial commit

---
+++ Front/eduardo-hartwig-portfolio/src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
